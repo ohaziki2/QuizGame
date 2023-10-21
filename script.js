dbec5b570366f840e7873dc875260156ad260f2c
ohaziki2 <haruya0014@gmail.com>
2023-10-21 14:00:09 +0900

    kousin

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ let sum;
 let correctAnswer;
 
 const questions = [
-    { question: '〇を押せますか？', answer: 1 },
-    { question: '世界4第通貨のうち「ドル、ユーロ、円」最後は「英ポンド」である', answer: 1 },
-    { question: 'ハワイにある火山のうち「キ」から始まる火山は「キラウェア」火山であるs', answer: 0 },
-    { question: '世界遺産にある「グランドキャニオン」の中央を流れる川の名は「ミシシッピ川」であsる   。', answer: 1 },
-    { question: 'ギザの3第ミラミッドの一番小さいピラミッドの名はカフラー王のピラミッドである', answer: 0 },
-    { question: '深海の定義は水深300M以下である。', answer: 0 },
-    { question: '火星の衛星はフォボスとダイノスである。', answer: 0 },
-    { question: '世界で初めて宇宙に到達した人間の国籍はアメリカである', answer: 0 },
-    { question: 'Suicaのチャージ上限は20000である。', answer: 1 },
-    { question: '天の川銀河から最も近い銀河はアンドロメダ銀河である。', answer: 1 },
-    { question: '「OK」の語源は「All Clear」を「Oll Klear」と間違えたからである', answer: 0 }
+    { question: 'Question1', answer: 1 },
+    { question: 'Question1', answer: 1 },
+    { question: 'Question1', answer: 0 },
+    { question: 'Question1', answer: 1 },
+    { question: 'Question1', answer: 0 },
+    { question: 'Question1', answer: 0 },
+    { question: 'Question1', answer: 0 },
+    { question: 'Question1', answer: 0 },
+    { question: 'Question1', answer: 1 },
+    { question: 'Question1', answer: 1 },
+    { question: 'Question1', answer: 0 }
 ];
 
 function writeQuestion() {
