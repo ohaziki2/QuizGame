7c4a35e53354ae6a58c7a9ea52980a094a15637b
ohaziki2 <haruya0014@gmail.com>
2023-12-09 14:46:29 +0900

    Update result.js

--- result.js
+++ result.js
@@ -2,5 +2,5 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const correctAnswer = urlParams.get('correctAnswer');
 const resultText = document.getElementsByClassName('resultscreen');
-resultText[0].textContent = `11問中/` + correctAnswer + '門正解！！';
+resultText[0].textContent = `11問中` + correctAnswer + '問正解！！';
 
