1b547d7eec09963e148cd9b45edd4bcd3847ecf3
ohaziki2 <haruya0014@gmail.com>
2024-01-13 15:44:47 +0900

    完成なはず

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const questions = [
     { question: 'Question1', answer: 1 , hintcontent: '' },
     { question: 'Question2', answer: 2 , hintcontent: '' },
     { question: 'Question3', answer: 3 , hintcontent: '' },
-    { question: 'Question1', answer: 4 , hintcontent: '' },
-    { question: 'Question1', answer: 0 , hintcontent: '' },
-    { question: 'Question1', answer: 0 , hintcontent: '' },
-    { question: 'Question1', answer: 0 , hintcontent: '' },
-    { question: 'Question1', answer: 0 , hintcontent: '' },
-    { question: 'Question1', answer: 1 , hintcontent: '' },
-    { question: 'Question1', answer: 1 , hintcontent: '' },
-    { question: 'Question1', answer: 0 , hintcontent: '' }
+    { question: 'Question4', answer: 4 , hintcontent: '' },
+    { question: 'Question5', answer: 0 , hintcontent: '' },
+    { question: 'Question6', answer: 0 , hintcontent: '' },
+    { question: 'Question7', answer: 0 , hintcontent: '' },
+    { question: 'Question8', answer: 0 , hintcontent: '' },
+    { question: 'Question9', answer: 1 , hintcontent: '' },
+    { question: 'Question10', answer: 1 , hintcontent: '' },
+    { question: 'Question11', answer: 0 , hintcontent: '' }
 ];
 //questionには問題、answerには答えの番号、hintcontentは選択肢等を書いて
 //例{ question: '赤い果物は？', answer: 1 , hintcontent: '1, リンゴ 2,バナナ 3,ブドウ 4,オレンジ' },
