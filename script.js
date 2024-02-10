54e76bcd2b059fe2ed3bde158cb09cda94024cd8
ohaziki2 <haruya0014@gmail.com>
2024-02-10 15:01:45 +0900

    こんどこそ完成

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ let sum;
 let correctAnswer;
 
 const questions = [
-    { question: 'Question1', answer: 1 , hintcontent: '' },
-    { question: 'Question2', answer: 2 , hintcontent: '' },
-    { question: 'Question3', answer: 3 , hintcontent: '' },
-    { question: 'Question4', answer: 4 , hintcontent: '' },
-    { question: 'Question5', answer: 0 , hintcontent: '' },
-    { question: 'Question6', answer: 0 , hintcontent: '' },
-    { question: 'Question7', answer: 0 , hintcontent: '' },
-    { question: 'Question8', answer: 0 , hintcontent: '' },
-    { question: 'Question9', answer: 1 , hintcontent: '' },
-    { question: 'Question10', answer: 1 , hintcontent: '' },
-    { question: 'Question11', answer: 0 , hintcontent: '' }
-];
-//questionには問題、answerには答えの番号、hintcontentは選択肢等を書いて
+    { question: 'Question1', answer: 1 , hintcontent: '' , choise1: 'a', choise2: 'i', choise3: 'u', choise4: 'e'},
+    { question: 'Question2', answer: 2 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+    { question: 'Question3', answer: 3 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+    { question: 'Question4', answer: 4 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+    { question: 'Question5', answer: 0 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+    { question: 'Question6', answer: 0 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+    { question: 'Question7', answer: 0 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+    { question: 'Question8', answer: 0 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+    { question: 'Question9', answer: 1 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+    { question: 'Question1', answer: 1 , hintcontent: '' , choise1: '', choise2: '', choise3: '', choise4: ''},
+] 
+//questionには問題、answerには答えの番号、hintcontentはヒントを書いて
 //例{ question: '赤い果物は？', answer: 1 , hintcontent: '1, リンゴ 2,バナナ 3,ブドウ 4,オレンジ' },
 function writeQuestion() {
     console.log(titleText)
@@ -25,6 +24,10 @@ function writeQuestion() {
     questionText.textContent = questions[sum].question;
     //hint[0].textContent = questions[sum].hintcontent;
     document.getElementsByClassName('fukidashi')[0].textContent = questions[sum].hintcontent;
+    document.getElementsByClassName('info1')[0].textContent = questions[sum].choise1
+    document.getElementsByClassName('info2')[0].textContent = questions[sum].choise2
+    document.getElementsByClassName('info3')[0].textContent = questions[sum].choise3
+    document.getElementsByClassName('info4')[0].textContent = questions[sum].choise4
 }
 
 function onAnswer(ans) {
